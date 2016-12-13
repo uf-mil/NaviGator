37749f3ef67c190e84eb9994e182e5f889c04680
Tess <tesshbianchi@gmail.com>
2016-12-12 20:34:06 -1000

    FIND THE BREAK: mission

--- mission_systems/setup.py
+++ mission_systems/setup.py
@@ -3,10 +3,9 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['navigator_testing_suite']
+    packages=['navigator_find_the_break', "navigator_test_perception"]
 )
 
 setup(**setup_args)
