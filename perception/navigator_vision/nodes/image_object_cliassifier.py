a570c3ed76538529532a90a2ee3979ad53f12ca4
RustyBamboo <dvolya@gmail.com>
2018-04-07 15:42:41 -0400

    OBJECTS: find closest object to ray projection and set id

--- perception/navigator_vision/nodes/image_object_cliassifier.py
+++ perception/navigator_vision/nodes/image_object_cliassifier.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python
 import numpy as np
 import rospy
 import image_geometry
 import mil_tools
 from sensor_msgs.msg import CameraInfo, Image
 import tf
+from mil_msgs.srv import ObjectDBQuery, ObjectDBQueryResponse
+from mil_msgs.msg import PerceptionObject
+
 
 class ImageGetter(object):
     def __init__(self, topic_name):
         self.sub = mil_tools.Image_Subscriber(topic_name, self.get_image)
 
-        print 'getting topic', topic_name
+        print ('getting topic', topic_name)
         self.frame = None
         self.done = False
 
@@ -23,7 +27,6 @@ class ImageGetter(object):
 
 class ObjectImageClassifier(object):
     def __init__(self):
-        print ("cool")
         self.camera_topics = ["/camera/front/left/image_rect_color", "/camera/seecam/image_rect_color"]
         self.camera_tfs = ["/front_left_cam", "/seecam"]
 
@@ -31,8 +34,11 @@ class ObjectImageClassifier(object):
 
         while None in [p.img_geom for p in self.cameras] and not rospy.is_shutdown():
             print (self.cameras)
-            print "Waiting frames..."
+            print ("Waiting frames...")
             rospy.sleep(1)
+
+        rospy.wait_for_service('/database/requests')
+        self.DBQuery_proxy = rospy.ServiceProxy('/database/requests', ObjectDBQuery)
 
         while not rospy.is_shutdown():
             try:
@@ -46,31 +52,63 @@ class ObjectImageClassifier(object):
                 cam_p, cam_q = zip(*enu_cam_tf)
                 cam_p = [np.array(p) for p in cam_p]
                 cam_r = [tf.transformations.quaternion_matrix(q)[:3, :3] for q in cam_q]
-
+                all_objects = None
+                try:
+                    all_objects = self.DBQuery_proxy(name="all", cmd="").objects
+                    # print("called")
+                    # print(all_objects)
+                except rospy.ServiceException, e:
+                    print ("Couldn't call database")
+                    rospy.sleep(5)
+                    continue
+                if all_objects is None:
+                    print ("Didn't find any objects")
+                    rospy.sleep(5)
+                    continue
                 for i in range(len(self.cameras)):
                     points, labels = info[i]
                     for j in range(len(points)):
-                        ray = self.get_ray_in_enu(points[j], self.cameras, enu_cam_tf[i])
-
+                        ray = self.get_ray_in_enu(points[j], self.cameras[i].img_geom, cam_r[i])
+                        if ray is None:
+                            continue
+                        # print("hey")
+                        o = self.find_closest_object_given_ray(all_objects, ray, cam_p[i])
+                        if o is not None:
+                            self.set_label_for_object(labels[j], o)
+                    rospy.sleep(2)
                         # find intersection in database and pray it works
-
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException, TypeError) as e:
                 print e
                 rospy.logwarn("TF link not found.")
                 rospy.sleep(.5)
                 continue
 
+
     def get_label_and_points(self, img):
         points = [np.array([50, 50]), np.array([200, 200])]
         labels = ["buoy", "totem"]
         return (points, labels)
 
-    def get_ray_in_enu(self, point, camera, enu_cam_tf):
+    def get_ray_in_enu(self, point, camera, R):
         # find ray and then put into inu
-        ray = np.array(camera.projectPixelTo3dRay((point)))
-        enu_cam_tf.transform_vector(ray)
-
-
+        ray = np.array(camera.projectPixelTo3dRay(point))
+        return R.dot(ray)
+        # return enu_cam_tf.transform_vector(ray)
+    def find_closest_object_given_ray(self, objects, ray, ray_base, tol = 3):
+        for o in objects:
+            distance = np.linalg.norm(np.cross(ray, mil_tools.rosmsg_to_numpy(o.pose.position) - ray_base))
+            # print (distance)
+            if distance > tol:
+                continue
+            else:
+                return o
+        return None
+    def set_label_for_object(self, label, o):
+        try:
+            all_objects = self.DBQuery_proxy(name="", cmd="{}={}".format(o.id, label)).objects
+            # print ("Set {} to {}".format(o.id, label))
+        except rospy.ServiceException, e:
+            print ("Couldn't set {} to {}".format(o.id, label))
 
 if __name__ == "__main__":
     rospy.init_node("singleshot_detector")
