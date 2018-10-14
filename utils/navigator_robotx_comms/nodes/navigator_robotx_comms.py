db6f1f0b1c5c81c489c05d9b773f3afbc846ca6b
Boris Ermakov-Spektor <b.ermakovspektor@ufl.edu>
2018-10-13 21:50:12 -0700

    First full implementation of robotx communication protocol.. No tests have been made yet, still a WIP, but ready for testing.

--- utils/navigator_robotx_comms/nodes/navigator_robotx_comms.py
+++ utils/navigator_robotx_comms/nodes/navigator_robotx_comms.py
@@ -4,45 +4,53 @@
 RobotX Communications: A node that handles communications
 with the RobotX Technical Director network.
 """
-import errno
+
+import datetime
+import math
+import socket
+import time
 
 import rospy
-from nav_msgs.msg import Odometry
+import tf.transformations as trans
 from geometry_msgs.msg import PointStamped
-import socket
-import datetime
-import time
+from mil_tools import rosmsg_to_numpy
+from nav_msgs.msg import Odometry
+from navigator_msgs.srv import *
 
 rospy.init_node("robotx_comms")
 
+delim = ','
 
-class RobotXComms():
+
+class RobotXHeartbeatMessage():
     """
-    Handles communications
-    with the RobotX Technical Director network.
+    Handles formation and sending of heartbeat message.
     """
-    system_mode = None  # type: int
 
     def __init__(self):
 
+        self.message_id = "RXHRB"
         self.gps_array = None
         self.odom = None
         self.auv_status = 1
-        self.system_mode = 1
-        # self.TEAM_ID = rospy.get_param(team_id)
-        self.team_id = "GATOR"
+        self.system_mode = 2
 
+        rospy.Subscriber("lla", PointStamped, self.gps_coord_callback)
+        rospy.Subscriber("odom", Odometry, self.gps_odom_callback)
         # rospy.Subscriber("auv_status", String, self.auv_status_callback)
 
-    def heartbeat_msg(self):
+    def from_string(self, string):
 
-        message_id = "$RXHRB"
-        delim = ","
+        # TODO: Implement testing method for heartbeat message
+        pass
 
-        # HST is 10 hours behind UTC
-        hst_time = datetime.datetime.utcnow() - datetime.timedelta(hours=10, minutes=0)
-        date_string = hst_time.strftime("%d%m%y")
-        time_string = hst_time.strftime("%H%M%S")
+    def to_string(self):
+
+        global delim
+        global team_id
+        global get_hst_date_time
+
+        hst_date_time = get_hst_date_time()
 
         if self.gps_array is not None:
             latitude = self.gps_array.point.x
@@ -53,39 +61,58 @@ class RobotXComms():
 
         if self.odom is not None:
             quaternion = self.odom.pose.pose.orientation
+            quaternion_numpy = rosmsg_to_numpy(quaternion)
+            euler_angles = trans.euler_from_quaternion(quaternion_numpy)
             north_south = ""
             east_west = ""
-            if quaternion.x > 0:
-                east_west = "E"
-            elif quaternion.x < 0:
-                east_west = "W"
-            if quaternion.y > 0:
+
+            if euler_angles[2] > 0 and euler_angles[2] < math.pi / 2:
                 north_south = "N"
-            elif quaternion.y < 0:
+                east_west = "E"
+            elif euler_angles[2] > math.pi / 2 and euler_angles[2] < math.pi:
+                north_south = "N"
+                east_west = "W"
+            elif euler_angles[2] < -math.pi / 2 and euler_angles[2] > -math.pi:
                 north_south = "S"
+                east_west = "W"
+            elif euler_angles[2] < 0 and euler_angles[2] > -math.pi / 2:
+                north_south = "S"
+                east_west = "E"
+
+        # TODO: EULER ANGLES pi to -pi, in radians, get degrees, fix orientiation.
+
         else:
             east_west = ""
             north_south = ""
 
-        checksum = "06"
+        first_half_data = "{0}{1}{2}{3}{4}{5}{6}".format(self.message_id,
+                                                         delim,
+                                                         hst_date_time,
+                                                         delim,
+                                                         latitude,
+                                                         delim,
+                                                         north_south)
 
-        msg_return = "{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}{12}{13}{14}{15}{16}{17}{18}*{19}\r\n".format(message_id,
-                                                                                                          delim,
-                                                                                                          date_string,
-                                                                                                          delim,
-                                                                                                          time_string,
-                                                                                                          delim,
-                                                                                                          latitude,
-                                                                                                          delim,
-                                                                                                          north_south,
-                                                                                                          delim,
-                                                                                                          longitude,
-                                                                                                          delim,
-                                                                                                          east_west,
-                                                                                                          delim,
-                                                                                                          self.team_id,
-                                                                                                          delim, str(
-                self.system_mode), delim, str(self.auv_status), checksum)
+        second_half_data = "{0}{1}{2}{3}{4}{5}{6}{7}{8}".format(longitude,
+                                                                delim,
+                                                                east_west,
+                                                                delim,
+                                                                team_id,
+                                                                delim, str(self.system_mode), delim,
+                                                                str(self.auv_status))
+
+        full_data = first_half_data + delim + second_half_data
+
+        # Test data
+        # full_data = 'RXHRB,101218,161229,21.31198,N,157.88972,W,AUVSI,2,1'
+
+        tot_checksum = 0
+        for unitCounter in range(len(full_data)):
+            tot_checksum = ord(full_data[unitCounter]) ^ tot_checksum
+
+        checksum = tot_checksum
+
+        msg_return = "${0}*{1}\r\n".format(full_data, str(checksum).zfill(2))
 
         return msg_return
 
@@ -102,6 +129,228 @@ class RobotXComms():
         self.odom = odom
 
 
+class RobotXEntranceExitGateMessage():
+    """
+    Handles formation and sending of entrance and exit gates message
+    """
+
+    def __init__(self):
+
+        self.message_id = "RXGAT"
+
+    def from_string(self, string):
+
+        # TODO: Implement testing method for entrance and exit gates message
+        pass
+
+    def to_string(self, data):
+
+        global delim
+        global team_id
+        global get_hst_date_time
+
+        hst_date_time = get_hst_date_time()  # type: str
+
+        light_buoy_active_letter = "N"
+
+        if data.light_buoy_active:
+            light_buoy_active_letter = "Y"
+
+        data = "{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}{12}".format(self.message_id,
+                                                                   delim,
+                                                                   hst_date_time,
+                                                                   delim,
+                                                                   team_id,
+                                                                   delim,
+                                                                   str(data.entrance_gate),
+                                                                   delim,
+                                                                   str(data.exit_gate),
+                                                                   delim,
+                                                                   light_buoy_active_letter,
+                                                                   delim,
+                                                                   data.light_pattern, )
+
+        # Test data
+        # full_data = '$RXGAT,101218,161229,AUVSI,1,2,Y,RBG*25'
+
+        tot_checksum = 0
+        for unitCounter in range(len(data)):
+            tot_checksum = ord(data[unitCounter]) ^ tot_checksum
+
+        checksum = tot_checksum
+
+        msg_return = "${0}*{1}\r\n".format(data, str(checksum).zfill(2))
+
+        send_message(msg_return)
+
+        return MessageExtranceExitGateResponse(msg_return)
+
+
+class RobotXScanCodeMessage():
+    """
+    Handles formation and sending of scan the code message
+    """
+
+    def __init__(self):
+        self.message_id = "RXCOD"
+
+    def from_string(self, string):
+        # TODO: Implement testing method for scan the code message
+        pass
+
+    def to_string(self, data):
+        global delim
+        global team_id
+        global get_hst_date_time
+
+        hst_date_time = get_hst_date_time()  # type: str
+
+        data = "{0}{1}{2}{3}{4}{5}{6}".format(self.message_id,
+                                              delim,
+                                              hst_date_time,
+                                              delim,
+                                              team_id,
+                                              delim,
+                                              data.light_pattern)
+
+        # Test data
+        # full_data = '$RXCOD,101218,161229,AUVSI,RBG*49'
+
+        tot_checksum = 0
+        for unitCounter in range(len(data)):
+            tot_checksum = ord(data[unitCounter]) ^ tot_checksum
+
+        checksum = tot_checksum
+
+        msg_return = "${0}*{1}\r\n".format(data, str(checksum).zfill(2))
+
+        send_message(msg_return)
+
+        return MessageScanCodeResponse(msg_return)
+
+
+class RobotXIdentifySymbolsDockMessage():
+    """
+    Handles formation and sending of identify symbols and dock message
+    """
+
+    def __init__(self):
+        self.message_id = "RXDOK"
+
+    def from_string(self, string):
+        # TODO: Implement testing method for identify symbols and dock message
+        pass
+
+    def to_string(self, data):
+        global delim
+        global team_id
+        global get_hst_date_time
+
+        hst_date_time = get_hst_date_time()  # type: str
+
+        data = "{0}{1}{2}{3}{4}{5}{6}{7}{8}".format(self.message_id,
+                                                    delim,
+                                                    hst_date_time,
+                                                    delim,
+                                                    team_id,
+                                                    delim,
+                                                    data.shape_color,
+                                                    delim,
+                                                    data.shape)
+
+        # Test data
+        # full_data = 'RXDOK,101218,161229,AUVSI,R,TRIAN*28'
+
+        tot_checksum = 0
+        for unitCounter in range(len(data)):
+            tot_checksum = ord(data[unitCounter]) ^ tot_checksum
+
+        checksum = tot_checksum
+
+        msg_return = "${0}*{1}\r\n".format(data, str(checksum).zfill(2))
+
+        send_message(msg_return)
+
+        return MessageIdentifySymbolsDockResponse(msg_return)
+
+
+class RobotXDetectDeliverMessage():
+    """
+    Handles formation and sending of detect and deliver message
+    """
+
+    def __init__(self):
+        self.message_id = "RXDEL"
+
+    def from_string(self, string):
+        # TODO: Implement testing method for detect and deliver message
+        pass
+
+    def to_string(self, data):
+        global delim
+        global team_id
+        global get_hst_date_time
+
+        hst_date_time = get_hst_date_time()  # type: str
+
+        data = "{0}{1}{2}{3}{4}{5}{6}{7}{8}".format(self.message_id,
+                                                    delim,
+                                                    hst_date_time,
+                                                    delim,
+                                                    team_id,
+                                                    delim,
+                                                    data.shape_color,
+                                                    delim,
+                                                    data.shape)
+
+        # Test data
+        # full_data = 'RXDEL,101218,161229,AUVSI,R,CIRCL*32'
+
+        tot_checksum = 0
+        for unitCounter in range(len(data)):
+            tot_checksum = ord(data[unitCounter]) ^ tot_checksum
+
+        checksum = tot_checksum
+
+        msg_return = "${0}*{1}\r\n".format(data, str(checksum).zfill(2))
+
+        send_message(msg_return)
+
+        return MessageDetectDeliverResponse(msg_return)
+
+
+def get_hst_date_time():
+    # HST is 10 hours behind UTC
+    hst_time = datetime.datetime.utcnow() - datetime.timedelta(hours=10, minutes=0)
+    date_string = hst_time.strftime("%d%m%y")
+    time_string = hst_time.strftime("%H%M%S")
+    return date_string + delim + time_string
+
+
+def send_message(message):
+    global TCP_IP
+    global TCP_PORT
+    global socketConnection
+    global connected
+    while not rospy.is_shutdown():
+        try:
+            socketConnection.send(message)
+            break
+        except socket.error:
+            print('Connection to TD Server Lost, Attempting Reconnection')
+            connected = False
+            # recreate socket
+            socketConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            while not connected:
+                # attempt to reconnect, otherwise sleep for 2 seconds
+                try:
+                    socketConnection.connect((TCP_IP, TCP_PORT))
+                    connected = True
+                    print("Re-connection to TD Server Successful")
+                except socket.error:
+                    time.sleep(2)
+
+
 if __name__ == "__main__":
 
     # INIT TCP
@@ -109,16 +358,30 @@ if __name__ == "__main__":
     TCP_PORT = rospy.get_param('td_port')
     BUFFER_SIZE = 1024
     connected = False
-
     socketConnection = None
 
-    robot_x_comms = RobotXComms()
-    rospy.Subscriber("lla", PointStamped,  robot_x_comms.gps_coord_callback)
-    rospy.Subscriber("odom", Odometry, robot_x_comms.gps_odom_callback)
+    # Constants used in multiple messages
+    # team_id = rospy.get_param(team_id)
+    team_id = "GATOR"
+
+    robot_x_heartbeat_message = RobotXHeartbeatMessage()
+
+    robot_x_entrance_exit_gate_message = RobotXEntranceExitGateMessage()
+    robot_x_scan_code_message = RobotXScanCodeMessage()
+    robot_x_identify_symbols_dock_message = RobotXIdentifySymbolsDockMessage()
+    robot_x_detect_deliver_message = RobotXDetectDeliverMessage()
+
+    service_entrance_exit_gate_message = rospy.Service('entrance_exit_gate_message', MessageExtranceExitGate,
+                                                       robot_x_entrance_exit_gate_message.to_string)
+    service_scan_code_message = rospy.Service('scan_code_message', MessageScanCode, robot_x_scan_code_message.to_string)
+    service_identify_symbols_dock_message = rospy.Service('identify_symbols_dock_message', MessageIdentifySymbolsDock,
+                                                          robot_x_identify_symbols_dock_message.to_string)
+    service_detect_deliver_message = rospy.Service('detect_deliver_message', MessageDetectDeliver,
+                                                   robot_x_detect_deliver_message.to_string)
 
     while not rospy.is_shutdown():
         if not connected:
-            print 'Attempting Connection to TD Server'
+            print('Attempting Connection to TD Server')
         while not connected:
             connected = False
             # recreate socket
@@ -127,29 +390,14 @@ if __name__ == "__main__":
             try:
                 socketConnection.connect((TCP_IP, TCP_PORT))
                 connected = True
-                print 'Connection to TD Server Successful'
+                print('Connection to TD Server Successful')
             except socket.error:
                 time.sleep(2)
-        heartbeatMsg = robot_x_comms.heartbeat_msg()
-        while True:
-            print "Sending Heartbeat!"
-            try:
-                socketConnection.send(heartbeatMsg)
-                break
-            except socket.error:
-                print 'Connection to TD Server Lost, Attempting Reconnection'
-                connected = False
-                # recreate socket
-                socketConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                while not connected:
-                    # attempt to reconnect, otherwise sleep for 2 seconds
-                    try:
-                        socketConnection.connect((TCP_IP, TCP_PORT))
-                        connected = True
-                        print( "Re-connection to TD Server Successful" )
-                    except socket.error:
-                        time.sleep(2)
-        time.sleep(1)
+        while not rospy.is_shutdown():
+            print('Sending Heartbeat!')
+            heartbeat_msg = robot_x_heartbeat_message.to_string()
+            send_message(heartbeat_msg)
+            time.sleep(1)
 
     if rospy.is_shutdown():
         socketConnection.close()
--- utils/navigator_robotx_comms/test/robotx_comms_test.py
+++ utils/navigator_robotx_comms/test/robotx_comms_test.py
@@ -5,49 +5,60 @@ import signal
 import sys
 import rospy
 
-msg_delimiter = ","
+if __name__ == "__main__":
 
-TCP_IP = rospy.get_param('td_ip')
-TCP_PORT = rospy.get_param('td_port')
-BUFFER_SIZE = 1024
+    msg_delimiter = ','
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((TCP_IP, TCP_PORT))
-server.listen(5)
+    TCP_IP = rospy.get_param('td_ip')
+    # TCP_IP = '192.168.37.234'
+    # TCP_PORT = rospy.get_param('td_port')
+    TCP_PORT = 1337
+    BUFFER_SIZE = 1024
 
-conn, addr = server.accept()
-print 'Connection address: ', addr
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((TCP_IP, TCP_PORT))
+    server.listen(5)
 
-
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    conn.close()
-    server.close()
-    sys.exit(1)
+    conn, addr = server.accept()
+    print 'Connection address: ', addr
 
 
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
+    def signal_handler(sig, frame):
+        print 'You pressed Ctrl+C!'
+        conn.close()
+        server.close()
+        sys.exit(1)
 
-timesRan = 0
 
-while True:
-    if timesRan >= 100:
-        print 'Test Success: Heartbeat received from NaviGator 100 times.'
-        sys.exit(0)
-    data = conn.recv(BUFFER_SIZE)  # type: str
-    if data:
-        dataList = data.split(msg_delimiter)
-        if dataList[0] == '$RXHRB':
-            timesRan += 1
-            print 'Print #:', timesRan
-            print 'Message ID:', dataList[0]
-            print 'HST Date:', dataList[1]
-            print 'HST Time:', dataList[2]
-            print 'Latitude:', dataList[3]
-            print 'N/S Indicator:', dataList[4]
-            print 'Longitude:', dataList[5]
-            print 'E/W Indicator:', dataList[6]
-            print 'Team ID:', dataList[7]
-            print 'System Mode:', dataList[8]
-            print 'AUV Status * Checksum:', dataList[9]
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    timesRan = 0
+
+    while True:
+        data = conn.recv(BUFFER_SIZE)  # type: str
+        if data:
+            data_list = data.split(msg_delimiter)
+            checksum_list = data.split("*")
+            checksum = checksum_list[1]
+
+            tot_checksum = 0
+            full_data_for_checksum = checksum_list[0][1:]
+            for unit_counter in range(len(full_data_for_checksum)):
+                tot_checksum = ord(full_data_for_checksum[unit_counter]) ^ tot_checksum
+            calc_checksum = str(tot_checksum).zfill(2) + '\r\n'
+            good_checksum = checksum == calc_checksum
+            if data_list[0] == '$RXHRB':
+                timesRan += 1
+                print 'Print #:', timesRan
+                print 'Checksum Validity:', good_checksum
+                print 'Message ID:', data_list[0]
+                print 'HST Date:', data_list[1]
+                print 'HST Time:', data_list[2]
+                print 'Latitude:', data_list[3]
+                print 'N/S Indicator:', data_list[4]
+                print 'Longitude:', data_list[5]
+                print 'E/W Indicator:', data_list[6]
+                print 'Team ID:', data_list[7]
+                print 'System Mode:', data_list[8]
+                print 'AUV Status * Checksum:', data_list[9]
